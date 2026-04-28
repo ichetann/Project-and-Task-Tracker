7aed7c02d39be9407d9015772a18aa78c9f7f36a
ichetann <chetanchaudhari617@gmail.com>
2026-04-28 13:38:10 +0530

    env for frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 // Create axios instance
 const api = axios.create({
